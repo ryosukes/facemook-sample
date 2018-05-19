ff0101508b9c62631003d269182007a377932fd4
ryosukes <nu.ciger@gmail.com>
2018-05-19 11:44:55 +0900

    create first migration files

---
+++ database/migrations/2018_05_19_110128_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->unsignedInteger('department_id');
+            $table->string('image_uri', 255);
+            $table->string('nickname', 255);
+            $table->string('slack_user_id', 255);
+            $table->timestamps();
+
+            $table->index('slack_user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
